a0388dc0d41fd780dd5ff45bfe6eeaa8d665e8e9
JohnShubin <jntshubin@gmail.com>
2024-11-15 19:16:06 -0500

    added grid

--- whackamole.py
+++ whackamole.py
@@ -2,6 +2,8 @@ import pygame
 
 #added my first comm
 def main():
+    x_val = 0
+    y_val = 0
     try:
         pygame.init()
         # You can draw the mole with this snippet:
@@ -14,7 +16,14 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(event.pos)
             screen.fill("light green")
+            for x_val in range(0, 640, 16):
+                pygame.draw.line(screen, "dark blue", (x_val, 0), (x_val, 512))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
+            for y_val in range(0, 512, 16):
+                pygame.draw.line(screen, "dark blue", (0, y_val), (640, y_val) )
             pygame.display.flip()
             clock.tick(60)
     finally:
